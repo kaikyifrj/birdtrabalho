6cfa6802c8cfd6ce11fe0b5cd1f134ef5a36cd55
kaikyifrj <kaikyaraujo160@gmail.com>
2025-07-16 23:27:22 -0300

    Create canvas.js

---
+++ canvas.js
@@ -0,0 +1,13 @@
+
+const eCanvas = document.querySelector("canvas");
+const eCtx = eCanvas.getContext("2d");
+
+function setup(){
+    eCanvas.width = 320;
+    eCanvas.height = 480;
+}
+setup();
+
+
+
+
