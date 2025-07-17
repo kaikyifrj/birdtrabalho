148fb39efab1ee2482b17818566774d074f9974a
kaikyifrj <kaikyaraujo160@gmail.com>
2025-07-16 23:29:32 -0300

    Create ground.js

---
+++ ground.js
@@ -0,0 +1,9 @@
+const ground = {
+    color:"lightgreen",
+    width: 320,
+    height: 90,
+    draw(){
+        eCtx.fillStyle = this.color;
+        eCtx.fillRect(0,450,this.width,this.height)
+    }
+}
